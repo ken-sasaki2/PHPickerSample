e960e4ee492a6f2371fb9d4d025e8cfd8f8b1e97
佐々木　謙 <nkekisasa222@gmail.com>
2023-07-15 17:59:30 +0900

    update: PHPicerで選択した画像を取得

--- PHPickerSample/ViewController.swift
+++ PHPickerSample/ViewController.swift
@@ -6,14 +6,65 @@
 //
 
 import UIKit
+import PhotosUI
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    private var picker: PHPickerViewController?
+    private var configuration = PHPickerConfiguration(photoLibrary: PHPhotoLibrary.shared())
+    private var selection = [String: PHPickerResult]()
+    private var selectedAssetIdentifiers = [String]()
+    private var selectedAssetIdentifierIterator: IndexingIterator<[String]>?
+    private var identifier = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setUp()
     }
-
-
+    
+    private func setUp() {
+        configuration.filter = nil
+        configuration.preferredAssetRepresentationMode = .current
+        configuration.selection = .ordered
+        configuration.selectionLimit = 0
+        configuration.preselectedAssetIdentifiers = selectedAssetIdentifiers
+        
+        picker = PHPickerViewController(configuration: configuration)
+        picker?.delegate = self
+    }
+    
+    @IBAction func onOpenButtonTapped(_ sender: UIButton) {
+        guard let picker = picker else {
+            return
+        }
+        present(picker, animated: true)
+    }
 }
 
+extension ViewController: PHPickerViewControllerDelegate {
+    
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+        let existingSelection = self.selection
+        var newSelection = [String: PHPickerResult]()
+
+        results.forEach { result in
+            guard let identifier = result.assetIdentifier else {
+                return
+            }
+            newSelection[identifier] = existingSelection[identifier]
+        }
+        
+        // Track the selection in case the user deselects it later.
+        selection = newSelection
+        selectedAssetIdentifiers = results.map(\.assetIdentifier!)
+        selectedAssetIdentifierIterator = selectedAssetIdentifiers.makeIterator()
+        
+        
+        print("kenken", selectedAssetIdentifiers)
+        print("kenken", selectedAssetIdentifierIterator)
+        
+        dismiss(animated: true)
+    }
+}
+ 
